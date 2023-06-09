f15b519a3398cdbbe06d0c597776bccb76c3e4e6
Thomas Rijpstra <thomas@fourlights.nl>
2023-06-09 18:32:07 +0200

    feat: use nitro to load polyfill at the earliest

---
+++ src/runtime/server/plugins/polyfill.ts
@@ -0,0 +1,38 @@
+import { joinURL, cleanDoubleSlashes } from 'ufo'
+import { useRuntimeConfig } from "#imports";
+
+/**
+ * Due to an upstream bug in Nuxt 3 we need to stub the `defineNitroPlugin`.
+ * See https://github.com/nuxt/nuxt/issues/18556
+ *
+ * Once this issue is resolved, then this file can be updated to the following:
+ *
+ * ```ts
+ * import { defineNitroPlugin } from '#imports'
+ * ```
+ */
+import { NitroApp } from 'nitropack'
+
+export type NitroAppPlugin = (nitro: NitroApp) => void
+export function defineNitroPlugin (def: NitroAppPlugin): NitroAppPlugin {
+  return def
+}
+/**
+ * End of workaround
+ */
+
+export default defineNitroPlugin((nitroApp) => {
+  const config = useRuntimeConfig()
+
+  const { src, isSelfHost } = config.public.nupolyon
+  // Prepend the current runtime value of app.baseURL if configured for self-hosting
+  const host = !isSelfHost ? src : cleanDoubleSlashes(joinURL(config.app.baseURL, src))
+
+  if (host) {
+    // NOTE: By setting `nomodule` we make sure it executed directly upon loading, and also works in browsers which don't support ESM.
+    const polyfill = `<script src="${host}" crossorigin="anonymous" nomodule></script>`
+    nitroApp.hooks.hook('render:html', (html) => {
+      html.head.unshift(polyfill)  // insert at the beginning of the array
+    })
+  }
+})
